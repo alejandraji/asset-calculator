7d7474a117c5c0df04cd16269ff1a44f3ce13173
alejandra <axjimenez89@gmail.com>
2025-01-12 15:26:32 -0500

    add currency filter

--- src/components/Calculator.vue
+++ src/components/Calculator.vue
@@ -1,6 +1,15 @@
 <template>
   <form class="" @submit.prevent="submitHandler">
     <div class="user-input">
+      <div>
+        <label for="currency">Choose currency:</label>
+        <select v-model="selectedCurrency" name="currency" id="currency">
+          <option v-for="currency in currencies" :value="currency.cc">
+            {{currency.cc}}
+          </option>
+        </select>
+        <div>Selected: {{ selectedCurrency }}</div>
+      </div>
       <label for="assets">Investable assets</label>
       <input type="text" v-model="assets" id="assets" name="assets" placeholder="$0">
     </div>
@@ -18,11 +27,14 @@
 
 <script setup>
   import { conversionToCrypto } from './../utils.js';
+  import currencies from './../currencies.json';
   import { ref } from 'vue';
 
+  const selectedCurrency = ref('USD');
   const assets = ref(null)
   const btc = ref(null)
   const eth = ref(null)
+  console.log(currencies)
 
   const submitHandler = async () => {
     const conversion = await conversionToCrypto(assets.value);
@@ -30,4 +42,6 @@
     btc.value = conversion.BTC;
     eth.value = conversion.ETH;
   }
+
+
 </script>
